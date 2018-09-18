85024b60ceb7702c1ec3e8624084d9da3f7c2327
rawan <rawaan_ali97@hotmail.com>
2018-09-18 12:43:44 +0200

    3nd commit

--- app/src/main/java/com/example/rawan/junkfoodtracker/Room/UserProductDao.kt
+++ app/src/main/java/com/example/rawan/junkfoodtracker/Room/UserProductDao.kt
@@ -12,7 +12,16 @@ interface UserProductDao{
     @Query("Select * from UserProduct")
     fun loadAllUsersWithProducts(): List<UserProductEntity>
 
-    @Query("SELECT * FROM product , user ,UserProduct where product.pid=UserProduct.userId AND user.id=UserProduct.productId")
+    @Query("Select counter from UserProduct where userId=:userId AND productBarcode=:productBarcode")
+    fun loadToUpdateCounter(userId:Int,productBarcode:Long): Int
+
+    @Query("UPDATE UserProduct SET counter=:counter  WHERE userId=:id AND productBarcode=:barcode")
+    fun Update(counter:Int,id:Int,barcode:Long)
+
+    @Query("select counter from UserProduct WHERE userId=:id AND productBarcode=:barcode")
+    fun sameUserSameProduct(id:Int,barcode:Long):Int
+
+    @Query("SELECT * FROM product , user ,UserProduct where product.barcode=UserProduct.userId AND user.id=UserProduct.productBarcode")
     fun loadAllData(): List<AllData>
 
     @Insert
